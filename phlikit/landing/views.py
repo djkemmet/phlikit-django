3d1d885d57c4be261218586558b982d0abf4feb0
DJ Kemmet <kemmetdaniel@gmail.com>
2020-10-25 12:40:06 -0500

    Got some templates started.
    Time to start building the landing page.

--- phlikit/landing/views.py
+++ phlikit/landing/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'landing/index.html')
+    return render(request, 'landing/landing_page.html')
