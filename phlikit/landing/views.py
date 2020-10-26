0fc2fa71d3233f08d42ab82625bd5275687a3003
DJ Kemmet <kemmetdaniel@gmail.com>
2020-10-25 20:26:38 -0500

    mailinglist signup form works need to put it in template.

--- phlikit/landing/views.py
+++ phlikit/landing/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import MailinglistSignUpForm
 
 # Create your views here.
 def index(request):
     return render(request, 'landing/landing_page.html')
+
+def update_MailinglistSignUp(request):
+
+    form = MailinglistSignUpForm()
+
+    if request.method == 'POST':
+        form = MailinglistSignUpForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+        
+            return redirect('/signup/')
+        else:
+            print(form.errors)
+
+    return render(request, 'landing/MailinglistSignUpForm.html', {'form': form})
