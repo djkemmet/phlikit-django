7d13ceab94686377ab6fd93e3e42ad80685a1eb7
DJ Kemmet <kemmetdaniel@gmail.com>
2020-11-25 16:33:47 -0600

    Closes #5
    This commit enables me to close issue #5. Solution is that django.forms provides a label property can I can set and then have it display as the template is rendered.

--- phlikit/landing/forms.py
+++ phlikit/landing/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from .models import MailinglistSignUp
 
 class MailinglistSignUpForm(forms.ModelForm):
-    email_address = forms.EmailField()
-    name = forms.CharField()
+    email_address = forms.EmailField(label="Email", initial="Enter your email.")
+    name = forms.CharField(label="Name", initial="What should we call you?")
 
 
     class Meta:
